fd45551adb3c84f49f00ccfa9ecc0d32b8d0d7b7
Karcioff <roberto.pivetta96@outlook.it>
2016-11-24 21:17:30 +0100

    prima prova

---
+++ config.php
@@ -0,0 +1,23 @@
+<?php
+
+//generali
+ 
+$sito_internet = "Pulmins";
+ 
+$data =(date("d-m-y"));
+ 
+$vers = "2.0";
+ 
+//URL PER HTACCESS
+ 
+$base_url = "http://localhost/Pulmins";
+ 
+//connessione DB
+ 
+$host = "127.0.0.1";
+ 
+$db_user = "pulmins_user";
+ 
+$db_psw = "U5ceyafPNhE2barc";
+ 
+$db_name = "pulmins";
