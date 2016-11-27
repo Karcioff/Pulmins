61dd64db741f7a68f011e94c0a47ed569815079d
Karcioff <roberto.pivetta96@outlook.it>
2016-11-27 20:16:50 +0100

    corrette librerie

--- index.php
+++ index.php
@@ -106,10 +106,10 @@ include("connessione_db.php"); // includere la connessione al database
         <!-- ================================================
           Scripts
           ================================================ -->
-        <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-        <script type="text/javascript" src="js/materialize.min.js"></script>
+        <script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+        <script type="text/javascript" src="js/bin/materialize.min.js"></script>
         <!--materialize js-->
-        <script type="text/javascript" src="js/materialize.js"></script>
+        
         <!--prism-->
         <script type="text/javascript" src="js/prism.js"></script>
 
