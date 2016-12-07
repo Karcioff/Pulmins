9d40c146c235235322fe5323a3328dd4bba7cd61
Karcioff <roberto.pivetta96@outlook.it>
2016-12-07 15:42:54 +0100

    aggiunta id centrocosto

--- commessa.php
+++ commessa.php
@@ -6,21 +6,24 @@
 class Commessa {
     public $committente;
     public $note;
-    public $id;
     public $percorsi;
+    public $id;
+    public $idCentroCosto;
     
     /**
      * costruttore per oggetto commessa
-     * @param type $unCommittente committente
-     * @param type $delleNote eventuali note
-     * @param type $unId id univoco della commessa
-     * @param type $deiPercorsi array di percorsi
+     * @param type string $unCommittente committente
+     * @param type string $delleNote eventuali note
+     * @param type array $deiPercorsi array di percorsi
+     * @param type int $unId id univoco della commessa
+     * @param type int $unIdCentroCosto id del centro di costo di appartenenza
      */
-    public function __construct($unCommittente,$delleNote,$unId,$deiPercorsi) {
+    public function __construct($unCommittente,$delleNote,$deiPercorsi,$unId,$unIdCentroCosto) {
         $this->committente=$unCommittente;
         $this->note=$delleNote;
         $this->id=$unId;
         $this->percorsi=$deiPercorsi;
+        $this->idCentroCosto=$unIdCentroCosto;
     }
 }
 
