fdbbbfdfc425c4586deaf24f9e43f96d475ce9d0
Karcioff <roberto.pivetta96@outlook.it>
2017-02-20 19:57:32 +0100

    modifiche login

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include("connessione_db.php"); 
+include("connessioneDB_php/connessione_db.php"); 
 $_SESSION["username"]=$_POST["username"];
 $_SESSION["password"]=$_POST["password"];
 $query = $connect->query("SELECT * FROM login WHERE username='".$_SESSION["username"]."' AND password ='".sha1($_SESSION["password"])."'");
@@ -8,7 +8,7 @@ if ($query->num_rows > 0) {
 	$_SESSION["logged"] =true;
 	$data = $query->fetch_assoc();
 	$_SESSION["utente"]=$data["IdUtente"];
-    header("Location:main.php");
+    header("Location:main_php/main.php");
 } else {
     header("Location: http://www.google.com");
 }
