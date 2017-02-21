dfbfb58472fb3f61ca32f45ab3d8fde54a0308f7
Karcioff <roberto.pivetta96@outlook.it>
2017-02-21 15:03:56 +0100

    correzioni main

--- main_php/main.php
+++ main_php/main.php
@@ -2,7 +2,6 @@
     <head>
         <title>The Materialize Collapsible Example</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">      
-        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
         <link rel="stylesheet" href="../css/materialize.css">
 
     </head>
@@ -10,18 +9,7 @@
         <div class="row">
             <div class="col s12 m6 l4" id="colonnaLista">
                 <ul class="collapsible popout" id="listaAppalti" data-collapsible="accordion">
-                    <li>
-                        <div class="collapsible-header"><i class="material-icons">filter_drama</i>First Section</div>
-                        <div class="collapsible-body"><p>This is first section.</p></div>
-                    </li>
-                    <li>
-                        <div class="collapsible-header"><i class="material-icons">place</i>Second Section</div>
-                        <div class="collapsible-body"><p>This is second section.</p></div>
-                    </li>
-                    <li>
-                        <div class="collapsible-header"><i class="material-icons">whatshot</i>Third Section</div>
-                        <div class="collapsible-body"><p>This is third section.</p></div>
-                    </li>
+                   
                 </ul>
             </div>
             <div class="col s12 m6 l8" id="panel">
@@ -29,11 +17,13 @@
             </div>
         </div>
 
-        <script type="text/javascript" src="../js/jquery-3.1.1.min.js"></script>           
+        <script src="../js/jquery-3.1.1.min.js"></script>           
         <script src="../js/bin/materialize.min.js"></script>
         <script src="../js/custom/mainUtils.js"></script>
         <script>
-            setListaAppalti(1);
+            $(document).ready(function () {
+                setListaAppalti(1);               
+            });
         </script>
     </body>  
 </html>
